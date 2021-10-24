51cf42c585c971472acd5160c37d2f6dab752120
Ilia <samoilovilia@mail.ru>
2021-10-24 19:16:27 +0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+        <img src="https://i.pinimg.com/originals/0f/41/d0/0f41d0404ae75869088e7c150affab84.png" alt="Logo"/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+
+        <div>
+        <img src="https://avatars.mds.yandex.net/i?id=140ef19292aa3f720656a089832abd3e-4714015-images-thumbs&n=13&exp=1" alt="Image" />
+        </div>
+
+        <div>
+          ava + description
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+
+          <div>
+            post 1
+          </div>
+
+          <div>
+            post 2
+          </div>
+        </div>
+
+      </div>
+    )
+}
+
+export default Profile;
